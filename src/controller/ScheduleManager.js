c4a52b0ead7acd243433ee5f019373156301acad
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 17:33:48 +0900

    feat: 비상 근무표 생성 기능 추가가

--- src/controller/ScheduleManager.js
+++ src/controller/ScheduleManager.js
@@ -17,6 +17,60 @@ class ScheduleManager {
     this.#schedule = new Schedule();
   }
 
+  createSchedule(month, day) {
+    this.#month = month;
+    const date = this.getDateByMonth(month);
+    let dayIndex = DAY.indexOf(day);
+    for (let today = 1; today <= date; today++) {
+      if (this.isHoliday(month, today)) {
+        if (
+          this.#schedule.isOverlapBeforeWorker(
+            this.#holidayWorkers[this.#holidayIndex]
+          )
+        ) {
+          this.#schedule.addSchedule(
+            `${month}월 ${today}일 ${DAY[dayIndex]}(휴일) ${
+              this.#holidayWorkers[this.#holidayIndex + 1]
+            }`
+          );
+          dayIndex = (dayIndex + 1) % DAY.length;
+          this.#schedule.addSchedule(
+            `${month}월 ${today + 1}일 ${DAY[dayIndex]}(휴일) ${
+              this.#holidayWorkers[this.#holidayIndex]
+            }`
+          );
+          this.#holidayIndex =
+            (this.#holidayIndex + 2) % this.#holidayWorkers.length;
+        } else {
+          this.#schedule.addSchedule(
+            `${month}월 ${today}일 ${DAY[dayIndex]}(휴일) ${
+              this.#holidayWorkers[this.#holidayIndex]
+            }`
+          );
+          this.#holidayIndex =
+            (this.#holidayIndex + 1) % this.#holidayWorkers.length;
+        }
+      } else if (this.isWeekend(dayIndex)) {
+        this.#schedule.addSchedule(
+          `${month}월 ${today}일 ${DAY[dayIndex]} ${
+            this.#holidayWorkers[this.#holidayIndex]
+          }`
+        );
+        this.#holidayIndex =
+          (this.#holidayIndex + 1) % this.#holidayWorkers.length;
+      } else {
+        this.#schedule.addSchedule(
+          `${month}월 ${today}일 ${DAY[dayIndex]} ${
+            this.#weekdaysWorkers[this.#weekIndex]
+          }`
+        );
+        this.#weekIndex = (this.#weekIndex + 1) % this.#weekdaysWorkers.length;
+      }
+      dayIndex = (dayIndex + 1) % DAY.length;
+    }
+    return this.#schedule;
+  }
+
   isWeekend(dayIndex) {
     if (!(dayIndex >= 0 && dayIndex <= 4)) return true;
     return false;
