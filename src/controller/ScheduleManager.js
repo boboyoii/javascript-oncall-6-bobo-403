da9fd715745fb7605c4985d409b670757cf08d93
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 17:32:53 +0900

    feat: 날짜가 법정휴무일인지 확인하는 기능 추가

--- src/controller/ScheduleManager.js
+++ src/controller/ScheduleManager.js
@@ -1,4 +1,28 @@
+import { DAY, HOLIDAY_DATE } from '../constant/DayConstant.js';
+import Schedule from '../model/Schedule.js';
+
 class ScheduleManager {
+  #weekdaysWorkers;
+  #holidayWorkers;
+  #weekIndex;
+  #holidayIndex;
+  #month;
+  #schedule;
+
+  constructor(weekdaysWorkers, holidayWorkers) {
+    this.#weekdaysWorkers = weekdaysWorkers;
+    this.#holidayWorkers = holidayWorkers;
+    this.#weekIndex = 0;
+    this.#holidayIndex = 0;
+    this.#schedule = new Schedule();
+  }
+
+  isHoliday(month, today) {
+    if (HOLIDAY_DATE[month] !== undefined)
+      if (HOLIDAY_DATE[month].includes(today)) return true;
+    return false;
+  }
+
   getDateByMonth(month) {
     if ([2].includes(month)) return 28;
     if ([4, 6, 9, 11].includes(month)) return 30;
