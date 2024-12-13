5217e6daa1e73d9be15a5b3d2a413f6565563dd7
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 15:12:43 +0900

    feat: 요일이 월-일 로 입력되었는지 확인하는 기능 추가

--- src/controller/Validator.js
+++ src/controller/Validator.js
@@ -1,9 +1,16 @@
-import { ERROR_MESSAGE } from '../constant/Message';
+import DAY from '../constant/DayConstant.js';
+import { ERROR_MESSAGE } from '../constant/Message.js';
 
 class Validator {
   validateMonth(month) {
     if (!(Number.isInteger(month) && month >= 1 && month <= 12)) {
       throw new Error(ERROR_MESSAGE.invalid_month);
+    }
+  }
+
+  validateDayOfWeek(day) {
+    if (!DAY.includes(day)) {
+      throw new Error(ERROR_MESSAGE.invalid_day_of_week);
     }
   }
 }
