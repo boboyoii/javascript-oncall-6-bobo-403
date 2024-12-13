8c0a22019459bf946c0d55ce206c86e2b18e3adc
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 16:18:05 +0900

    feat: 근무자가 직전과 중복되는지 여부를 알려주는 기능 추가가

--- src/model/Schedule.js
+++ src/model/Schedule.js
@@ -12,6 +12,14 @@ class Schedule {
   showSchedule() {
     return this.#schedule.join('\n');
   }
+
+  isOverlapBeforeWorker(worker) {
+    const regex = new RegExp(`${worker}`);
+    if (regex.test(this.#schedule[this.#schedule.length - 1])) {
+      return true;
+    }
+    return false;
+  }
 }
 
 export default Schedule;
