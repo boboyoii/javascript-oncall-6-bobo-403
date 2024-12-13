2df4e999529065066771f671ced6b76d9687aa2e
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 15:02:24 +0900

    feat: 진행 메시지 상수화화

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -7,3 +7,9 @@ export const ERROR_MESSAGE = Object.freeze({
   invalid_name_length: `${PREFIX_ERROR} 근무자 닉네임은 최대 5자 까지 가능 합니다.\n`,
   invalid_name_unique: `${PREFIX_ERROR} 근무자 닉네임이 중복됩니다.\n`,
 });
+
+export const PROGRESS_MESSAGE = Object.freeze({
+  enter_month_and_day: '비상 근무를 배정할 월과 시작 요일을 입력하세요> ',
+  enter_weekdays_worker: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+  enter_holiday_worker: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+});
