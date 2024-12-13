31cb0e8dd280928eb2eab0ef2609cf2c146dc73c
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 17:31:55 +0900

    fix: 잘못된 함수명 호출 수정

--- src/view/UserOuput.js
+++ src/view/UserOuput.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 class UserOutput {
   printSchedule(schedule) {
-    Console.print(schedule.showSchedul());
+    Console.print(schedule.showSchedule());
   }
 }
 
