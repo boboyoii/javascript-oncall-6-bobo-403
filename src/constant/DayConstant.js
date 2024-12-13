827cb5437a36b252f86ba36dbf1ff5f4b9a7536f
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-12-13 16:25:27 +0900

    feat: 휴일 날짜 상수화

--- src/constant/DayConstant.js
+++ src/constant/DayConstant.js
@@ -1,3 +1,11 @@
-const DAY = ['월', '화', '수', '목', '금', '토', '일'];
+export const DAY = ['월', '화', '수', '목', '금', '토', '일'];
 
-export default DAY;
+export const HOLIDAY_DATE = Object.freeze({
+  1: [1],
+  3: [1],
+  5: [5],
+  6: [6],
+  8: [15],
+  10: [3, 9],
+  12: [25],
+});
