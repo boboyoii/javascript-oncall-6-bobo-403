0056815590e0e517c2271ce713050af2a674c11e
boyeon <boyeon6331@gamil.com>
2026-01-04 12:38:48 +0900

    feat: 다음 근무자를 구하는 기능 추가

--- src/model/Workers.js
+++ src/model/Workers.js
@@ -1,10 +1,23 @@
 class Workers {
   #names;
   #pos;
+  #skip;
 
   constructor(workers) {
     this.#names = workers;
     this.#pos = 0;
+    this.#skip = 0;
+  }
+
+  getNext(before) {
+    let next = this.#names[this.#pos];
+    if (before === next) {
+      this.#skip += 1;
+      return this.#names[(this.#pos + 1) % this.#names.length];
+    }
+    this.#pos = (this.#pos + this.#skip) % this.#names.length;
+    this.#skip = 0;
+    return next;
   }
 }
 
