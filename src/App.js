8509c13df09b637159aadf0bc7abc8c5a24e2cca
boyeon <boyeon6331@gamil.com>
2026-01-04 13:47:40 +0900

    feat: 잘못 입력할 경우 다시 입력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ScheduleController from './controller/ScheduleController.js';
+import retry from './utils/retry.js';
 import InputView from './view/InputView.js';
 import OutputView from './view/OuputView.js';
 
@@ -6,9 +7,9 @@ class App {
   scheduleController;
 
   async run() {
-    const [month, day] = await InputView.inputMonthAndDay();
-    const weekdayWorkers = await InputView.inputWeekdayOrder();
-    const holidayWorkers = await InputView.inputHolidayOrder();
+    const [month, day] = await retry(() => InputView.inputMonthAndDay());
+    const weekdayWorkers = await retry(() => InputView.inputWeekdayOrder());
+    const holidayWorkers = await retry(() => InputView.inputHolidayOrder());
 
     this.scheduleController = new ScheduleController(
       month,
